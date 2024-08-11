c8921e8f61c79a0b31a45cef2fabf364f6e26ed3
dan <dezzzl2580@gmail.com>
2024-08-11 18:45:21 +0300

    fix: config

--- src/main/java/org/scuni/userservice/UserServiceApplication.java
+++ src/main/java/org/scuni/userservice/UserServiceApplication.java
@@ -2,10 +2,12 @@ package org.scuni.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
+//@EnableFeignClients
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
